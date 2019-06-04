0bb5a304741b1cafb0114969a3ac259f92f77bfd
Matthew Gaudio <mattgaud2425@Gmail.com>
2019-06-04 19:52:22 -0400

    added scopes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ApplicationRecord
+
+  scope :fps, -> { where(game_type: 'fps') }
+  scope :moba, -> { where(game_type: 'moba') }
+  scope :hero, -> { where(game_type: 'hero') }
+  scope :more, -> { where(game_type: 'more') }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   include Clearance::User
+
+  scope :fps, -> { where(game_type: 'fps') }
+	scope :moba, -> { where(game_type: 'moba') }
+  scope :hero, -> { where(game_type: 'hero') }
+	scope :more, -> { where(game_type: 'more') }
 end
