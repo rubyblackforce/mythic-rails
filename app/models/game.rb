141c47e1cafa449bb1a812286fdff3f77687ad42
Matthew Gaudio <mattgaud2425@Gmail.com>
2019-06-06 14:55:03 -0400

    preliminary footer

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
 
-  scope :fps, -> { where(game_type: 'fps') }
-  scope :moba, -> { where(game_type: 'moba') }
-  scope :hero, -> { where(game_type: 'hero') }
-  scope :more, -> { where(game_type: 'more') }
+  scope :fps, -> { where(game_type: 'fps').order(game_group: :asc) }
+  scope :moba, -> { where(game_type: 'moba').order(game_group: :asc) }
+  scope :hero, -> { where(game_type: 'hero').order(game_group: :asc) }
+  scope :more, -> { where(game_type: 'more').order(game_group: :asc) }
 end
