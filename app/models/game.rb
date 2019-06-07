a4bc98ed24cdbaf98bd4b4239b90d9b53f1102de
Matthew Gaudio <mattgaud2425@Gmail.com>
2019-06-06 21:19:57 -0400

    changes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
 
-  scope :fps, -> { where(game_type: 'fps').order(game_group: :asc) }
-  scope :moba, -> { where(game_type: 'moba').order(game_group: :asc) }
-  scope :hero, -> { where(game_type: 'hero').order(game_group: :asc) }
-  scope :more, -> { where(game_type: 'more').order(game_group: :asc) }
+  scope :fps, -> { where(game_type: 'fps').order(game_group: :asc, long_title: :asc) }
+  scope :moba, -> { where(game_type: 'moba').order(game_group: :asc, long_title: :asc) }
+  scope :hero, -> { where(game_type: 'hero').order(game_group: :asc, long_title: :asc) }
+  scope :more, -> { where(game_type: 'more').order(game_group: :asc, long_title: :asc) }
 end
